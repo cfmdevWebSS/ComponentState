340cd34b7e190e98dedc56bd6d9055e7dc4381f7
Lawrence Buta <larry.buta@gmail.com>
2021-11-13 22:39:09 -0500

    Interim Save-Exercise Three-11-13-2021

--- src/Practice3.js
+++ src/Practice3.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-/* 
-  1. Setup UserForm to accept props
-  2. Display the proper values from props where needed
-*/
-const UserForm = () => (
+const UserForm = (props) => (
   <p>
-    <label htmlFor="PROPS_ID">PROPS_LABEL</label>:
-    <input id="PROPS_ID" type="text" onChange="PROPS_ONCHANGE" />
+    <label htmlFor={props.id}>{props.label}</label>:
+    <input id={props.id} type="text" onChange={props.onChange} />
   </p>
 );
 
@@ -21,10 +17,9 @@ class Practice3 extends React.Component {
     this.setState({ first: e.target.value });
   };
 
-  /* 
-    3. Create a handler function called handleLast    
-    4. Have the function update last in state to e.target.value
-  */
+  handleLast = e => {
+    this.setState({ last: e.target.value });
+  };
 
   render() {
     return (
@@ -32,16 +27,9 @@ class Practice3 extends React.Component {
         <h2>
           {this.state.first} {this.state.last}
         </h2>
-        {/*
-          3. Call <UserForm /> and pass in the following prop values
-              id = "firstName"
-              label = "First Name"
-              onChange = handleFirst
-          4. Call <UserForm /> again and pass in the following prop values
-              id = "lastName"
-              label = "Last Name"
-              onChange = handleLast              
-        */}
+
+        <UserForm id = "firstName" label = "First Name" onChange = {this.handleFirst} />
+        <UserForm id = "lastName" label = "Last Name" onChange = {this.handleLast} />
       </>
     );
   }
